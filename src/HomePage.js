05e9acefcb17c25461bb3fe13f9f901992a4b03a
Tobias Norton <tnorton20@yahoo.com>
2018-12-27 15:14:48 -0500

    Content added to main page

--- src/HomePage.js
+++ src/HomePage.js
@@ -23,8 +23,8 @@ class HomePage extends Component {
           <h1>What is Localist?</h1>
           <div className="box-container">
             <div className="box">
-              <p>Cultural Tourism</p>
-              <p>
+              <p className="header">Cultural Tourism</p>
+              <p className="body">
                 Localist provides a unique platform to meet with locals anywhere on the planet so
                 that travelers can get a richer experience and see things up close and personal.
                 Find the best street food, the local music scene, the best views, the hidden gems
@@ -32,8 +32,8 @@ class HomePage extends Component {
               </p>
             </div>
             <div className="box">
-              <p>Locals Can Hang Out With Travelers</p>
-              <p>
+              <p className="header">Locals Can Hang Out With Travelers</p>
+              <p className="body">
                 Show off your town! Remember, those from other countries don't have the same context
                 as you; your town might be cooler than you think, and it could be a great
                 opportunity to hang out with someone from another country. The best part is, it's
@@ -42,8 +42,8 @@ class HomePage extends Component {
               </p>
             </div>
             <div className="box">
-              <p>What Localist is Not</p>
-              <p>
+              <p className="header">What Localist is Not</p>
+              <p className="body">
                 Localist is not a dating website or a platform for couch sharing, but hey, it's a
                 free world! If you should choose to use it for such purposes, more power to you. We
                 just want Localists on both sides to have the best cultural experience they can.
@@ -55,17 +55,18 @@ class HomePage extends Component {
           <h1>How Does It Work?</h1>
           <div className="box-container">
             <div className="box">
-              <p>Create an Account</p>
-              <p>
+              <p className="header">Create an Account</p>
+              <p className="body">
                 You will be prompted to create an account with either Facebook or Google. From there
                 you will be prompted to create a profile. This is your chance
               </p>
             </div>
             <div className="box">
-              <p>Browse Locals</p>
+              <p className="header">Browse Locals</p>
             </div>
             <div className="box">
-              <p>Set Up a Time and Place to Meet</p>
+              <p className="header">Set Up a Time and Place to Meet</p>
+              <p className="body">Yada yada yadadaaa</p>
             </div>
           </div>
         </section>

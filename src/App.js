dea8cd13788433c746d30d5eeeda38f0cab37623
Tobias Norton <tnorton20@yahoo.com>
2018-12-27 10:48:29 -0500

    Request profile started

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css'
 import HomePage from './HomePage'
 import MatchedProfile from './MatchedProfile'
 import MyProfile from './MyProfile'
+import RequestProfile from './RequestProfile'
 
 class App extends Component {
   render() {
     return (
       <div>
         {/* <HomePage /> */}
-        <MatchedProfile />
+        {/* <MatchedProfile /> */}
         {/* <MyProfile /> */}
+        <RequestProfile />
       </div>
     )
   }
